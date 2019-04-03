8b2f55de1a4dee6b7db78fc37fb37eac9af6f6d7
Aubrey Portwood <aubreypwd@gmail.com>
2019-04-03 15:18:08 -0700

    Add documentation.

---
+++ loader.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+####
+ # Loader
+ #
+ # Add `source "$HOME/.bash/loader.bash"` to your ~/.bash_profile
+ #
+ # @since Wednesday, April 3, 2019
+ ##
+
+# Load partials.
+source "$HOME/.bash/general.bash"
+source "$HOME/.bash/aliases.bash"
+source "$HOME/.bash/functions.bash"
