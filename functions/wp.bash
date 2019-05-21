69fb8008b3b7fb6d5fb2ac90bae3fcd66d38f56f
Aubrey Portwood <code@aubreypwd.com>
2019-05-21 10:12:16 -0700

    Add way to do wp db import/export with .gz files.

--- functions/wp.bash
+++ functions/wp.bash
@@ -25,7 +25,7 @@ function wp-replace {
 function wp-debug {
 	if [ '--help' == "$1" ]; then
 		echo "Usage: wp-debug [--reset]"
-			return;
+		return;
 	fi
 
 	if [ '--reset' == "$1" ]; then
@@ -48,28 +48,13 @@ function wp-debug {
 }
 
 ###
- # Clear and view debug.log.
- #
- # @since 6/21/16
- ##
-function wp-debug-reset {
-	if [ -e ./debug.log ]; then
-		clear
-		echo "============== debug.log cleared ==============" > debug.log
-		wp-debug
-	else
-		echo "No debug.log found. :("
-	fi
-}
-
-###
  # Applies a Trac ticket patch via a URL.
  #
  # E.g: apply-patch <the url to the trac ticket>
  #
  # @since 4/5/16
  ##
-function apply-patch {
+function wp-apply-patch {
 	curl -k "$1" | patch -p0
 }
 
@@ -80,19 +65,8 @@ function apply-patch {
  #
  # @since Monday, April 1, 2019
  ##
-function make-patch {
+function wp-make-patch {
 	git diff --no-prefix master..HEAD > "$1".patch
-}
-
-###
- # An easy way to run wp search-replace.
- #
- # E.g: wpreplaced "example.com" "example.test" --url="example.com"
- #
- # @since Wednesday, April 17, 2019
- ##
-function wpreplaced {
-	wp search-replace "$1" "$2" --network --all-tables --url="$1"
 }
 
 ###
@@ -102,17 +76,56 @@ function wpreplaced {
  #
  # @since Wednesday, April 17, 2019
  ##
-function wpreplace {
-	wp search-replace "$1" "$2" --network --all-tables "$3" "$4" "$5" "$6"
+function wp-db-replace {
+	echo "Replacing $1 with $2 in all tables..." &&
+		wp search-replace "$1" "$2" --all-tables
+
+	if [ '--dbpass' == "$3" ]; then
+		wp-db-pass
+	fi
 }
 
 ###
  # Easy way to set all user's passwords to `password`.
  #
- # E.g: wpdbpass
+ # E.g: db-pass
  #
  # @since Wednesday, April 17, 2019
  ##
-function wpdbpass {
-	wp db query "UPDATE wp_users SET user_pass = '5f4dcc3b5aa765d61d8327deb882cf99';"
+function wp-db-pass {
+	echo "Changing all users' passwords to 'password'..." &&
+		wp db query "UPDATE wp_users SET user_pass = '5f4dcc3b5aa765d61d8327deb882cf99';"
+}
+
+###
+ # Export wp db export into a .gz file.
+ #
+ # @since Tuesday, May 21, 2019
+ #
+ # E.g: wp-export
+ ##
+function wp-export {
+	sqlfile=$(wp db export --porcelain)
+	gzip "$sqlfile"
+}
+
+###
+ # Run wp db import on a .gz file.
+ #
+ # @since Tuesday, May 21, 2019
+ #
+ # E.g: wp-import "$file"
+ ##
+function wp-import {
+	echo "Extracting $1..."
+	gunzip "$1"
+
+	noext=$(echo "$1" | cut -f 1 -d '.')
+	sqlfile="$noext.sql"
+
+	echo "Importing $sqlfile..."
+	wp db import "$sqlfile"
+
+	echo "Re-compressing $sqlfile > $1"
+	gzip "$sqlfile"
 }
